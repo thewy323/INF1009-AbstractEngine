dba53ed2257f397c66a44e150bb63b1f4a710475
NixonLeeDS <nixonthenlds@gmail.com>
2026-02-06 18:57:47 +0800

    Add falling object collision demo to simulator

--- core/src/main/java/com/inf1009/engine/manager/EntityManager.java
+++ core/src/main/java/com/inf1009/engine/manager/EntityManager.java
@@ -1,31 +1,32 @@
 package com.inf1009.engine.manager;
 
 import com.inf1009.engine.entity.AbstractGameEntity;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class EntityManager {
 
-    private final List<AbstractGameEntity> entityList = new ArrayList<>();
+    private final List<AbstractGameEntity> entities = new ArrayList<>();
 
     public void addEntity(AbstractGameEntity entity) {
-        entityList.add(entity);
+        entities.add(entity);
+    }
+
+    public void removeEntity(AbstractGameEntity entity) {
+        entities.remove(entity);
     }
 
     public void update(float dt) {
-        for (AbstractGameEntity e : entityList) {
+        for (AbstractGameEntity e : entities) {
             e.update(dt);
         }
-        entityList.removeIf(AbstractGameEntity::isDestroyed);
     }
 
     public List<AbstractGameEntity> getEntities() {
-        return entityList;
+        return entities;
     }
 
-
     public void clear() {
-
+        entities.clear();
     }
 }
--- core/src/main/java/com/inf1009/engine/scene/SimulatorScreen.java
+++ core/src/main/java/com/inf1009/engine/scene/SimulatorScreen.java
@@ -23,11 +23,15 @@ public class SimulatorScreen implements Screen {
     private Texture bg;
     private Texture playerATex;
     private Texture playerBTex;
+    private Texture itemTex;
 
     // entities
     private DynamicEntity agentA;
     private DynamicEntity agentB;
     private StaticEntity ground;
+    private DynamicEntity fallingItem;
+
+    private float fallSpeed = 180f;
 
     public SimulatorScreen(GameMaster game) {
         this.game = game;
@@ -41,19 +45,22 @@ public class SimulatorScreen implements Screen {
         bg = new Texture("img/bg.png");
         playerATex = new Texture("img/playerA.png");
         playerBTex = new Texture("img/playerB.png");
-
+        itemTex = new Texture("img/falling.png");
         // reset world
         game.getEntityManager().clear();
 
-        // entities
+        // players
         agentA = new DynamicEntity(80, 120, 32, 32, 220f);
         agentB = new DynamicEntity(140, 120, 32, 32, 220f);
 
+        // ground
         ground = new StaticEntity(0, 40, 640, 40);
 
         game.getEntityManager().addEntity(agentA);
         game.getEntityManager().addEntity(agentB);
         game.getEntityManager().addEntity(ground);
+
+        spawnFallingItem();
     }
 
     @Override
@@ -67,32 +74,59 @@ public class SimulatorScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // input + movement
+        // movement
         game.getMovementManager().applyInput(agentA, game.getIOManager().readP1(), dt);
         game.getMovementManager().applyInput(agentB, game.getIOManager().readP2(), dt);
 
-        // update
+        // update entities
         game.getEntityManager().update(dt);
 
-        // collision
+        // falling item movement
+        if (fallingItem != null) {
+            float ny = fallingItem.getY() - fallSpeed * dt;
+            fallingItem.setPosition(fallingItem.getX(), ny);
+
+            // respawn if missed
+            if (ny < -40) {
+                game.getEntityManager().removeEntity(fallingItem);
+                spawnFallingItem();
+            }
+        }
+
+        // collision system
         List<ICollidable> collidables = new ArrayList<>();
         game.getEntityManager().getEntities().forEach(e -> {
-            if (e instanceof ICollidable) collidables.add((ICollidable) e);
+            if (e instanceof ICollidable) {
+                collidables.add((ICollidable) e);
+            }
         });
         game.getCollisionManager().update(collidables);
+
+        // catch logic (generic demo)
+        if (fallingItem != null) {
+            if (agentA.getBounds().overlaps(fallingItem.getBounds())
+                    || agentB.getBounds().overlaps(fallingItem.getBounds())) {
+
+                game.getEntityManager().removeEntity(fallingItem);
+                spawnFallingItem();
+            }
+        }
 
         // render
         batch.begin();
 
-        // background
         batch.draw(bg, 0, 0, 640, 480);
 
-        // ground
-        batch.draw(playerATex, ground.getX(), ground.getY(), ground.getW(), ground.getH());
-
-        // players
         batch.draw(playerATex, agentA.getX(), agentA.getY(), agentA.getW(), agentA.getH());
         batch.draw(playerBTex, agentB.getX(), agentB.getY(), agentB.getW(), agentB.getH());
+
+        if (fallingItem != null) {
+            batch.draw(itemTex,
+                    fallingItem.getX(),
+                    fallingItem.getY(),
+                    fallingItem.getW(),
+                    fallingItem.getH());
+        }
 
         batch.end();
     }
@@ -104,7 +138,12 @@ public class SimulatorScreen implements Screen {
         bg.dispose();
         playerATex.dispose();
         playerBTex.dispose();
+        itemTex.dispose();
     }
 
-    
+    private void spawnFallingItem() {
+        float x = (float) (Math.random() * (640 - 32));
+        fallingItem = new DynamicEntity(x, 480, 32, 32, 0f);
+        game.getEntityManager().addEntity(fallingItem);
+    }
 }
