f66ab2e32941f50f0f202d23a9294b861671168b
NixonLeeDS <nixonthenlds@gmail.com>
2026-02-07 11:09:59 +0800

    update iMovable

--- core/src/main/java/com/inf1009/engine/entity/DynamicEntity.java
+++ core/src/main/java/com/inf1009/engine/entity/DynamicEntity.java
@@ -1,6 +1,6 @@
 package com.inf1009.engine.entity;
 
-public class DynamicEntity extends AbstractGameEntity implements Moveable, ICollidable {
+public class DynamicEntity extends AbstractGameEntity implements IMoveable, ICollidable {
 
     // Movement speed (horizontal movement)
     private float speed;
--- core/src/main/java/com/inf1009/engine/entity/IMoveable.java
+++ core/src/main/java/com/inf1009/engine/entity/IMoveable.java
@@ -1,6 +1,6 @@
 package com.inf1009.engine.entity;
 
-public interface Moveable {
+public interface IMoveable {
 
     // Apply input-based movement for this frame
     void movement(InputState input, float dt);
